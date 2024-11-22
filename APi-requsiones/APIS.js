2b40b2415e5988a8e6f6aefc3cda37b5607e7571
Deivi Ft alura <deivixerusgame@gmail.com>
2024-11-22 14:15:28 -0500

    Add files via upload

---
+++ APi-requsiones/APIS.js
@@ -0,0 +1,74 @@
+ const url= "https://673a923b339a4ce4451873d6.mockapi.io/productos"
+ 
+ async function productos(){
+
+   try{ const productos= await fetch(url)
+    const datos= await productos.json();
+    
+      console.log(datos)
+    return datos;
+
+   }catch{
+        console.log("Tenemos un erro, espera un rato")
+   }
+ } 
+ 
+ 
+
+
+
+
+async function AgregarProduct(titulo,precio,imagen) {
+
+  try{  const envios= await fetch(url, {
+
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+      titulo:titulo,
+      precio:precio,
+      imagen:imagen
+    })
+    
+
+  })
+    
+  const Datos= await envios.json();
+  
+
+  return Datos;
+
+  }catch{
+    console.log("datos no enviados correctamente");
+
+  }
+  
+}
+
+// delete
+
+export async function eliminarProducto(id) {
+    try {
+        const response = await fetch(`${url}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log('response:', response)
+
+        if (!response.ok) {
+            throw new Error('Error al eliminar el producto');
+        }
+    } catch (error) {
+        console.error('Error al conectar con la API', error);
+        throw error;
+    }
+}
+
+
+
+export const ConexionAPI={
+  productos,AgregarProduct,eliminarProducto 
+};
---
+++ APi-requsiones/Exp.js
@@ -0,0 +1,101 @@
+import {ConexionAPI} from "./APIS.js";
+
+
+const lista= document.querySelector("[data-lista]")
+
+function NuevoCard(titulo,precio,imagen,id){
+
+    const Product= document.createElement("li")
+    Product.className="card"
+    Product.setAttribute("data-id",id)
+
+    Product.innerHTML=`
+    <img class="imag_producto" src="${imagen}" alt="">
+                    <label class="">${titulo}</label>
+                    <div class="container__descripcion">
+                        
+                        <label class="precio">$${precio} cop</label>
+                        <button class="eliminar"></button>
+                    </div>
+    `
+    return Product;
+}
+
+async function Ejecutarcard() {
+
+   try{ const ListaBasedatos = await ConexionAPI.productos();
+
+    ListaBasedatos.forEach( productos=> {
+        
+   
+         lista.appendChild(NuevoCard(productos.titulo,productos.precio,productos.imagen, productos.id));
+
+        });
+
+        agregarEventosEliminar();
+   }catch{
+
+    console.log("error de conexion")
+   }
+    
+}
+
+Ejecutarcard();
+
+
+//AGREGAR PRODUCTO
+
+const formulario= document.querySelector("[data-form]")
+
+async function Agregar(evento){
+
+    evento.preventDefault();
+    formulario.reset();
+    const titulo=document.querySelector("[data-nombre]").value;
+    const precio=document.querySelector("[data-precio]").value;
+    const imagen=document.querySelector("[data-imagen]").value;
+
+
+    try{    await ConexionAPI.AgregarProduct(titulo,precio,imagen)
+        location.reload();
+
+    }catch{
+        alert("carga exitosa")
+    }
+
+} 
+
+formulario.addEventListener("submit", evento=> Agregar(evento))
+
+
+const limpiar= document.querySelector('.button')
+
+limpiar.addEventListener('click', (e)=>{
+
+    e.preventDefault();
+    formulario.reset();    
+
+})
+
+
+// ELIMINAR PRODUCTO
+
+function agregarEventosEliminar() { 
+    const botonesEliminar = document.querySelectorAll('.eliminar'); 
+    botonesEliminar.forEach(boton => { boton.addEventListener('click', async (e) => { 
+       
+        const tarjeta = e.target.closest('.card'); 
+        const id = tarjeta.getAttribute('data-id'); 
+
+        console.log(id)
+        
+        try { await ConexionAPI.eliminarProducto(id);
+             tarjeta.remove(); 
+
+        } catch (error) { 
+            console.error('No se pudo eliminar el producto', error); 
+        } 
+    }); 
+}); 
+
+}
